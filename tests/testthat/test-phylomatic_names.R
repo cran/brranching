eada11838ee62d18b25077c2bc22c60484d44db1
Scott Chamberlain <myrmecocystus@gmail.com>
2019-07-27 03:30:02 +0000

    version 0.5.0

--- tests/testthat/test-phylomatic_names.R
+++ tests/testthat/test-phylomatic_names.R
@@ -4,6 +4,8 @@ mynames <- c("Poa annua", "Salix goodingii", "Helianthus annuus")
 
 test_that("phylomatic_names - rsubmit works", {
   skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
 
   nms <- suppressMessages(phylomatic_names(mynames, format = 'rsubmit'))
 
@@ -14,6 +16,8 @@ test_that("phylomatic_names - rsubmit works", {
 
 test_that("phylomatic_names - isubmit (default) works", {
   skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
 
   nms <- suppressMessages(phylomatic_names(taxa = mynames, format = 'isubmit'))
 
@@ -25,6 +29,8 @@ test_that("phylomatic_names - isubmit (default) works", {
 
 test_that("phylomatic_names - db=apg works", {
   skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
 
   nms <- suppressMessages(phylomatic_names(mynames, db = "apg"))
 
@@ -36,6 +42,8 @@ test_that("phylomatic_names - db=apg works", {
 
 test_that("phylomatic fails as expected", {
   skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
 
   # fails when no taxa given
   expect_error(phylomatic_names(), "argument \"taxa\" is missing")
@@ -48,3 +56,34 @@ test_that("phylomatic fails as expected", {
   expect_error(phylomatic_names(mynames, db = "things"),
                "'arg' should be one of")
 })
+
+test_that("phylomatic_names behaves as expected if no Entrez env var set", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+  
+  myname <- "Salix goodingii"
+  mynames <- c("Salix goodingii", "Poa annua")
+
+  # message about key not given when key is set
+  expect_silent(phylomatic_names(myname))
+
+  env_var <- Sys.getenv("ENTREZ_KEY")
+  Sys.unsetenv("ENTREZ_KEY")
+
+  # message about the missing key should be given
+  # FIXME: ideally test that it's only given once per function call
+  # expect_message(phylomatic_names(myname), "No ENTREZ API key provided")
+
+  # only given once per function call
+  # expect_message(phylomatic_names(mynames), "No ENTREZ API key provided")
+
+  # test that it's only given once per function call
+  res <- conditionz::capture_message(phylomatic_names(mynames))
+  expect_equal(length(res$text), 1)
+
+
+  # reset env var
+  Sys.setenv("ENTREZ_KEY" = env_var)
+})
+
